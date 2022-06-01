d57e940f0db728c765a83d975d1632272eb5abcb
鬼谷子 <358683537@qq.com>
2022-06-01 17:02:57 +0800

    fix: remove caller.csproj from docker file and update image

--- src/Web/Masa.EShop.Web.Client/Pages/Catalog/CatalogDetails.razor.cs
+++ src/Web/Masa.EShop.Web.Client/Pages/Catalog/CatalogDetails.razor.cs
@@ -5,11 +5,11 @@ public partial class CatalogDetails : EShopPageBase
     private CatalogItem _catalogItem = new();
     private List<RelatedProduct> _relatedProducts = new()
     {
-        new() { Name = "GA406B 温热管线饮水机", Brand = "Lonsid", ImgUrl = "https://img-cdn.lonsid.co/image/1593360117.jpg", Price = 9999, Rating = 5 },
-        new() { Name = "G1管线饮水机", Brand = "Lonsid", ImgUrl = "https://img-cdn.lonsid.co/image/1593360094.jpg", Price = 9999, Rating = 5 },
-        new() { Name = "GA406K 速热管线饮水机", Brand = "Lonsid", ImgUrl = "https://img-cdn.lonsid.co/image/1555404598.jpg", Price = 9999, Rating = 5 },
-        new() { Name = "GT3桌面即热饮水机", Brand = "Lonsid", ImgUrl = "https://img-cdn.lonsid.co/image/1560130226.jpg", Price = 9999, Rating = 5 },
-        new() { Name = "GR320RB冷热型饮水机", Brand = "Lonsid", ImgUrl = "https://img-cdn.lonsid.co/image/1603728000ddBMgnpYFmMWTlAl3bAX179.jpg", Price = 9999, Rating = 5 }
+        new() { Name = "GA406B 温热管线饮水机", Brand = "Lonsid", ImgUrl = "./img/15.jpg", Price = 9999, Rating = 5 },
+        new() { Name = "G1管线饮水机", Brand = "Lonsid", ImgUrl = "./img/16.jpg", Price = 9999, Rating = 5 },
+        new() { Name = "GA406K 速热管线饮水机", Brand = "Lonsid", ImgUrl = "./img/15.jpg", Price = 9999, Rating = 5 },
+        new() { Name = "GT3桌面即热饮水机", Brand = "Lonsid", ImgUrl = "./img/17.jpg", Price = 9999, Rating = 5 },
+        new() { Name = "GR320RB冷热型饮水机", Brand = "Lonsid", ImgUrl = "./img/15.jpg", Price = 9999, Rating = 5 }
     };
 
     [Parameter]
